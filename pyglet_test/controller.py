fdc98be56ec6112e55463cb68fdbece2a196d41d
cisnerosa2000 <cisnerosa2000@gmail.com>
2015-09-22 17:40:58 -0500

    Pyglet

--- pyglet_test/controller.py
+++
@@ -1,9 +0,0 @@
-import pyglet
-
-joysticks = pyglet.input.get_joysticks()
-if joysticks:
-    joystick = joysticks[0]
-joystick.open()
-
-def on_joyhat_motion(joystick, hat_x, hat_y):
-    print hat_x,hat_y
