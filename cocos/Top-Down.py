c2254cfb3984586aebe4e37723c29b337a2fcd08
cisnerosa2000 <cisnerosa2000@gmail.com>
2015-09-28 14:43:44 -0500

    Last commit boi

--- cocos/Top-Down.py
+++ cocos/Top-Down.py
@@ -4,6 +4,7 @@ from cocos.layer import *
 from cocos.sprite import *
 from cocos.actions import *
 from cocos.particle_systems import *
+from cocos import shader
 
 import cocos.tiles
 import cocos.text
@@ -15,11 +16,15 @@ import math
 import numpy
 from heapq import *
 
+import random
+import inspect
+from multiprocessing import Process
+import subprocess
 
 
-#
-##
+
 ### Declaring some global variables that will be very useful later
+
 LEFT = 65361
 RIGHT = 65363
 UP = 65362
@@ -31,6 +36,8 @@ D = 100
 W = 119
 S = 115
 
+R = 114
+
 ONE = 49
 TWO = 50
 THREE = 51
@@ -40,9 +47,16 @@ FIVE = 53
 one_eighty_over_pi = 57.2957795131
 
 ### Declaring some global variables that will be very useful later
-##
-#
 
+
+
+class DeathLayer(Layer):
+    is_event_handler = True
+    def __init__(self):
+        super(DeathLayer, self).__init__()
+        self.ol = cocos.text.Label(text="Game Over!",position = (400,400),font_size=30,anchor_x ='center',anchor_y='center')
+        self.add(self.ol)
+        
 class MainMenu(cocos.menu.Menu):
 
     def __init__(self):
@@ -93,29 +107,28 @@ class OptionsMenu(cocos.menu.Menu):
     def on_show_fps(self, value):
         cocos.director.director.show_FPS = value
 
-class PlayerCollider(cocos.tiles.RectMapCollider):
-   def __init__(self, player):
-       self.player = player
 
-   def collide_bottom(self, dy):
-       print "bottom"
-   def collide_left(self, dx):
-       print "left"
-
-   def collide_right(self, dx):
-       print "right"
-
-   def collide_top(self, dy):
-       print "top"
 
 
 class Enemy(object):
-    def __init__(self,x,y):
+    def __init__(self,x,y,target):
         self.health = 3
         self.x = x
         self.y = y
         self.img = Sprite('resources/player_sprites/zombie.png',position = (self.x,self.y))
+        self.img.tag = "zombie"
+        target.batch.add(self.img)
         
+        self.img.cshape = cm.AARectShape(
+            self.img.position,
+            self.img.width//2,
+            self.img.height//2
+        )
+        target.collision_manager.add(self.img)
+        
+        
+       
+       
         
         
         
@@ -139,19 +152,50 @@ class Slot(object):
     
         
         
+class Bullet(object):
+    def __init__(self,x,y,x2,y2,target):
+        self.target = target
+        self.target.bullet_list.append(self)
+       
+        self.img = Sprite('resources/weapon_sprites/projectile.png',position=(x,y))
+        self.img.tag = "bullet"
+        target.bullet_batch.add(self.img)
+        x,y = 400+random.randint(-15,15),304+random.randint(-15,15)
+        
+        self.mouse_vector = [x-x2,y-y2]
+        self.c2 = self.mouse_vector[0] **2 + self.mouse_vector[1] **2
+        self.c = math.sqrt(self.c2)
+        self.norm = [self.mouse_vector[0]/self.c,self.mouse_vector[1]/self.c]
+        self.norm[0] *= -25
+        self.norm[1] *= -25
+        
+        self.img.cshape = cm.AARectShape(
+            self.img.position,
+            self.img.width//2,
+            self.img.height//2
+        )
+        target.collision_manager.add(self.img)
+        
+        
+        self.img.do(Repeat(MoveBy((self.norm[0],self.norm[1]),0)))
+        
+        
+
 class GameLayer(ScrollableLayer):
     is_event_handler = True
     def __init__(self):
+        
         super(GameLayer, self).__init__()
         self.schedule(self.update)
+        self.schedule_interval(self.spawn,.2)
         self.mouse_x,self.mouse_y = 0,0
         
+        director.interpreter_locals["game"] = self
+                
+        self.bullet_list = []
+        self.enemy_list = []
         
-    
-        
-        
-        #
-        ##
+      
         ### Load Map
         self.scroller = ScrollingManager()
         self.add(self.scroller,z=-1)
@@ -160,16 +204,15 @@ class GameLayer(ScrollableLayer):
         self.scroller.add(self.map,-1,'test')
         
         
-        
         ### Load Map
-        ##
-        #
-        
-        #
-        ##
+ 
         ### Inventory/HUD
+        self.max_ammo = 15
+        self.magazine = self.max_ammo
+        self.ammo = 30
         
-        self.hud_layer = cocos.layer.ColorLayer(255, 0, 0, 255, width=1, height=1)     
+        
+        self.hud_layer = cocos.layer.ColorLayer(255, 0, 0, 255, width=1, height=1)    
         self.hud_layer.opacity = 100   
         game_scene.add(self.hud_layer,z=1)
         
@@ -186,43 +229,39 @@ class GameLayer(ScrollableLayer):
             self.hud_layer.add(self.slots[slot].img)
         
         
+        
     
         self.kills = 0
-        self.kills_label = cocos.text.Label(text='%s Kills' % self.kills,position=(736,130),font_size=15)
+        self.kills_label = cocos.text.Label(text='%s Kills' % self.kills,position=(720,130),font_size=15,anchor_x='left')
         self.hud_layer.add(self.kills_label)
         
+        self.ammo_label = cocos.text.Label(text='%s/%s' %(self.magazine,self.ammo),position=(720,100),font_size=15,anchor_x='left')
+        self.health_label = cocos.text.Label(text='%d HP' %(100),position=(720,75),font_size=15,anchor_x='center')
+        self.hud_layer.add(self.ammo_label)
+        self.hud_layer.add(self.health_label)
         
         ### Inventory/HUD
-        ##
-        #
-        
-        #
-        ##
+
         ### Player 
         
-        self.player_sprite = Sprite('resources/player_sprites/player.png',position=(200,240))
+        self.player_sprite = Sprite('resources/player_sprites/player.png',position=(200,200))
+        self.player_sprite.health = 100
+        self.player_sprite.tag = "player"
         self.add(self.player_sprite)
         self.chars_pressed = set()
         
         
         
-        self.magazine = 30
-        self.ammo = 120
         
         self.rifle = Sprite("resources/weapon_sprites/rifle.png")
         self.player_sprite.add(self.rifle)
         self.rifle.do(MoveBy((10,4),0))
         
         
-        ###
-        ##
-        #
         
-        
-        #
-        ##
+        ### Player
+
         ### Collision
-        self.collider = PlayerCollider(self.player_sprite)
         
         self.collision_manager = cm.CollisionManagerBruteForce()
         self.player_sprite.cshape = cm.AARectShape(
@@ -235,25 +274,22 @@ class GameLayer(ScrollableLayer):
       
         
         ### Collision
-        ##
-        #
-        
-        
-        #
-        ##
+
+   
         ### Setting up enemy batch
         
         self.batch = cocos.batch.BatchNode()
-        self.add(self.batch,z=1)
+        self.map.add(self.batch,z=1)
+        
+        self.bullet_batch = cocos.batch.BatchNode()
+        self.map.add(self.bullet_batch,z=1)
+        
         
         ### Setting up enemy batch
-        ##
-        #
         
+        self.location = inspect.stack()[1][1]
+        self.soundtrack()
         
-        self.p = Sun()
-        self.p.position = (400,400)
-        self.map.add(self.p)
         
 
         
@@ -263,47 +299,120 @@ class GameLayer(ScrollableLayer):
         
     def update(self,dt):
         
+        if self.player_sprite.health < 1:
+            self.death()
         #gameloop
         
         x = self.player_sprite.position[0]
         y = self.player_sprite.position[1]
         
         
+      
+        ### Enemies / Collision
+        ex,ey = self.player_sprite.x*2,self.player_sprite.y*2
+        for enemy in self.enemy_list:
+            try:
+                vector = [ex-enemy.img.x,ey-enemy.img.x]
+                angle = math.atan2(*vector)
+                angle *= one_eighty_over_pi
+                enemy.img.do(RotateTo(angle-95,0))
+            
+                pvector = [enemy.img.x-ex,enemy.img.y-ey]
+                c2 = pvector[0] **2 + pvector[1] **2
+                c = math.sqrt(c2)
+                try:
+                    enemy.norm = [pvector[0]/c,pvector[1]/c]
+                    enemy.norm[0] *= -8
+                    enemy.norm[1] *= -8
+            
+                    enemy.img.do(MoveBy((enemy.norm[0],enemy.norm[1]),0))
+                except:
+                    pass
+                
+            
+                enemy.img.cshape.center = enemy.img.position
+                enemy.collisions = self.collision_manager.objs_colliding(enemy.img)
+                if enemy.collisions:
+                    for obj in enemy.collisions:
+                        if obj.tag == "zombie":
+                            evec = self.away_vector(enemy.x,enemy.y,obj.x,obj.y)
+                            obj.do(MoveBy((evec[0],evec[1]),0))
+                        elif obj.tag == "player" and self.player_sprite.health > 0:
+                            self.player_sprite.health -= 1
+                    
+                            
+                            
+                        elif obj.tag == "bullet":
+                            self.kills += 1
+                            self.ammo += 1 
+                            
+                            self.hud_layer.remove(self.kills_label)
+                            self.kills_label = cocos.text.Label(text='%s Kills' % self.kills,position=(720,130),font_size=15,anchor_x='left')
+                            self.hud_layer.add(self.kills_label)
+                            
+                            
+                            try:
+                                self.collision_manager.remove(obj)
+
+                            except:
+                                pass                         
+                            
+                            self.batch.remove(enemy.img)
+                            self.enemy_list.remove(enemy)
+                            self.collision_manager.remove(enemy.img)
+                            del enemy
+                            
+                            
+                            
+            except:
+                pass
+                    
+                        
+                
+                
+                        
+            
+            
         
-        #
-        ##
-        ### collision
-        
-        self.player_sprite.cshape.center = self.player_sprite.position
-        collisions = self.collision_manager.objs_colliding(self.player_sprite)
-        
-        if collisions:
-            print collisions
-        
-        
-        ### collision
-        ##
-        #
+        self.player_sprite.cshape.center = (ex,ey)
+        pcollisions = self.collision_manager.objs_colliding(self.player_sprite)
         
         
         
-        #
-        ##
+                
+        
+        
+        ### Enemies / Collision
+     
+        ### bullets
+        
+        for b_obj in self.bullet_list:
+            b_obj.dist = self.get_distance(b_obj.img.x,b_obj.img.y,self.player_sprite.x*2,self.player_sprite.y*2)
+            b_obj.img.cshape.center = b_obj.img.position
+            if b_obj.dist > 300:
+                try:
+                    self.collision_manager.remove(b_obj.img)
+                    self.bullet_list.remove(b_obj)
+                    self.bullet_batch.remove(b_obj.img)
+                    del b_obj.img
+                    del b_obj
+                except:
+                    pass
+           
+                        
+           
+        
+        ### bullets
+
         ### point player towards mouse
         self.player_vector = [self.mouse_x-400,self.mouse_y-304]
         self.angle = math.atan2(*self.player_vector)
         self.angle *= one_eighty_over_pi
         self.player_sprite.do(RotateTo(self.angle-95,0))
-        ### point player towards mouse
-        ##
-        #
-         
-         
-         
-        #
-        ##
-        ### move player and handle map collision
         
+        ### point player towards mouse 
+ 
+        ### move player and handle map collision
         
         if W in self.chars_pressed:
             y += 4
@@ -313,13 +422,10 @@ class GameLayer(ScrollableLayer):
             y -= 4
         if D in self.chars_pressed:
             x += 4
+        if R in self.chars_pressed:
+            self.reload()
     
       
-        
-        
-        
-        
-        
         self.player_sprite.position = x,y
        
         
@@ -331,15 +437,14 @@ class GameLayer(ScrollableLayer):
         self.scroller.get('test').set_view(0, 0, 800, 800, -x, -y)
              
         ### move player and handle map collision
-        ##
-        #
-        
-        
-        #
-        ##
+
         ### inventory 
-        self.kills_label = cocos.text.Label(text='%d Kills' % self.kills,position=(736,130),font_size=15)
         
+        
+        
+        self.hud_layer.remove(self.health_label)
+        self.health_label = cocos.text.Label(text='%d HP' %(self.player_sprite.health),position=(720,75),font_size=15,anchor_x='center')
+        self.hud_layer.add(self.health_label)
         
         for i in self.slots:
             self.hud_layer.remove(self.slots[i].img)
@@ -380,9 +485,11 @@ class GameLayer(ScrollableLayer):
             if self.slots[i].img not in self.hud_layer:
                 self.hud_layer.add(self.slots[i].img)
         
+       
+        
         ### inventory
-        ##
-        #
+ 
+     
         
         
        
@@ -390,14 +497,25 @@ class GameLayer(ScrollableLayer):
         
        
        
+    def spawn(self,dt):
+        if len(self.enemy_list) < 2:
+            enemy = Enemy(x=self.player_sprite.x+random.randint(-500,500),y=self.player_sprite.y+random.randint(-500,500),target=self)
+            self.enemy_list.append(enemy)
+    def death(self):
+        death_scene = Scene()
+        death_scene.add(DeathLayer())
+        director.push(death_scene)
+   
     def on_mouse_press (self, x, y, buttons, modifiers):
         self.fire()
+    def on_mouse_release(self, x, y, buttons, modifiers):
+        self.firing = False
         
     def on_mouse_motion(self,x,y,dx,dy):
         self.mouse_x,self.mouse_y = director.get_virtual_coordinates(x, y)
-
+    
     def on_mouse_drag (self, x, y, dx, dy, buttons, modifiers):
-        self.mouse_x,self.mouse_y = x,y       
+        self.mouse_x,self.mouse_y = x,y      
     def on_key_press(self,key,modifiers):
         try:
             self.chars_pressed.add(key)
@@ -408,17 +526,82 @@ class GameLayer(ScrollableLayer):
             self.chars_pressed.remove(key)
         except:
             pass
-    def fire(self):
-        pass
+    def fire(self):   
+        if self.magazine > 0:
+            x,y = self.point_to_world((self.player_sprite.x,self.player_sprite.y))
+            y,x = self.map.point_to_local((x,y))
+    
+            self.bullet = Bullet(x=y,y=x,x2=self.mouse_x,y2=self.mouse_y,target=self)
+            self.magazine -= 1
+            
+            self.hud_layer.remove(self.ammo_label)
+            self.ammo_label = cocos.text.Label(text='%s/%s' %(self.magazine,self.ammo),position=(700,100),font_size=15)
+            self.hud_layer.add(self.ammo_label)
+    def reload(self):
+        n = self.max_ammo - self.magazine
+        if self.ammo >= n:
+            self.ammo -= n
+            self.magazine += n
+        else:
+            self.magazine += self.ammo
+            self.ammo = 0
+            
+            
+            
+        self.hud_layer.remove(self.ammo_label)
+        self.ammo_label = cocos.text.Label(text='%s/%s' %(self.magazine,self.ammo),position=(700,100),font_size=15)
+        self.hud_layer.add(self.ammo_label)
+        
+  
+        
+        
+       
         
         
 
 
         
 
+
+    def away_vector(self,startx,starty,x2,y2):
+        try:
+            vector = [startx-x2,starty-y2]
+            c2 = vector[0] **2 + vector[1] **2
+            c = math.sqrt(c2)
+            norm_vector = [vector[0]/c,vector[1]/c]
+        
+            norm_vector[0] *= -2
+            norm_vector[1] *= -2
+        
+            return norm_vector
+        except:
+            return [0,0]
+
+
+    def get_distance(self,x1,y1,x2,y2):
+        return math.sqrt(((x1-x2)**2)+((y1-y2)**2))
+    def soundtrack(self):
+        def tunes():
+            if self.player_sprite.health > 0:
+                cannon_audio_file = self.location.replace("Top-Down.py","soundtrack/day %s.mp3" % random.randint(1,29))
+                boom = subprocess.call(["afplay", cannon_audio_file])
+                tunes()
+            
+            
+            
+        self.tunesp = Process(target=tunes)
+        self.tunesp.start()
 
 if __name__ == "__main__":
-    director.init(width=800,height=608,caption="Top-Down",fullscreen=False)
+    director.init(width=800,height=608,caption="Top-Down",fullscreen=False,resizable=True)
     scene = cocos.scene.Scene()
     scene.add(cocos.layer.MultiplexLayer(MainMenu(), OptionsMenu()), z=1)
     cocos.director.director.run(scene)
+    
+    
+    
+    
+    
+    
+    
+    
